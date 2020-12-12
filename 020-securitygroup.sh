da8ad60672f3c18fdab818b867a0738ca34ffdab
homma509 <kouki.honma@gmail.com>
2020-12-12 16:20:15 +0900

    add security group for vpc endpoint

--- 020-securitygroup.sh
+++ 020-securitygroup.sh
@@ -13,11 +13,15 @@ TENPLATE=020-securitygroup
 
 # 指定必須パラメータ
 # 実施環境のグローバルIPアドレスを'curl inet-ip.info'などを利用して、以下に指定してください。
-# 例）ADMIN_IPADDRESS=123.123.123.123/32
-ADMIN_IPADDRESS=123.123.123.123/32
+ADMIN_IPADDRESS=
+CLOUD9_SECURITY_GROUP_ID=
 
 if [ -z "${ADMIN_IPADDRESS}" ]; then
   echo "シェルの中でADMIN_IPADDRESSを指定してください。"
+  exit
+fi
+if [ -z "${CLOUD9_SECURITY_GROUP_ID}" ]; then
+  echo "シェルの中でCLOUD9_SECURITY_GROUP_IDを指定してください。"
   exit
 fi
 
@@ -29,6 +33,7 @@ CFN_TEMPLATE=templates/${TENPLATE}.yml
 aws cloudformation deploy --stack-name ${CFN_STACK_NAME} --template-file ${CFN_TEMPLATE} ${CHANGESET_OPTION} \
   --parameter-overrides \
   SystemName=${SYSTEM_NAME} \
-  AdminIpaddress=${ADMIN_IPADDRESS}
+  AdminIpaddress=${ADMIN_IPADDRESS} \
+  Cloud9SecurityGroupID=${CLOUD9_SECURITY_GROUP_ID}
 
 exit

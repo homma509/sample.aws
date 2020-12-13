dd7733b32958cf94f7109b53bff8165403c2fb13
homma509 <kouki.honma@gmail.com>
2020-12-13 13:55:56 +0900

    comment out Cloud9 Security Group ID

--- 020-securitygroup.sh
+++ 020-securitygroup.sh
@@ -15,16 +15,16 @@ TENPLATE=020-securitygroup
 # 実施環境のグローバルIPアドレスを'curl inet-ip.info'などを利用して、以下に指定してください。
 # 例）ADMIN_IPADDRESS=123.123.123.123/32
 ADMIN_IPADDRESS=123.123.123.123/32
-CLOUD9_SECURITY_GROUP_ID=xxxxxid
+# CLOUD9_SECURITY_GROUP_ID=xxxxxid
 
 if [ -z "${ADMIN_IPADDRESS}" ]; then
   echo "シェルの中でADMIN_IPADDRESSを指定してください。"
   exit
 fi
-if [ -z "${CLOUD9_SECURITY_GROUP_ID}" ]; then
-  echo "シェルの中でCLOUD9_SECURITY_GROUP_IDを指定してください。"
-  exit
-fi
+# if [ -z "${CLOUD9_SECURITY_GROUP_ID}" ]; then
+#   echo "シェルの中でCLOUD9_SECURITY_GROUP_IDを指定してください。"
+#   exit
+# fi
 
 # テンプレート実行用パラメータ
 CFN_STACK_NAME=${SYSTEM_NAME}-${TENPLATE}
@@ -34,7 +34,7 @@ CFN_TEMPLATE=templates/${TENPLATE}.yml
 aws cloudformation deploy --stack-name ${CFN_STACK_NAME} --template-file ${CFN_TEMPLATE} ${CHANGESET_OPTION} \
   --parameter-overrides \
   SystemName=${SYSTEM_NAME} \
-  AdminIpaddress=${ADMIN_IPADDRESS} \
-  Cloud9SecurityGroupID=${CLOUD9_SECURITY_GROUP_ID}
+  AdminIpaddress=${ADMIN_IPADDRESS}
+  # Cloud9SecurityGroupID=${CLOUD9_SECURITY_GROUP_ID}
 
 exit
